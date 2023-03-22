e4214494ed190cd69cdea37361261e571b02264a
Zach <zacbenincasa@gamil.com>
2023-03-21 20:21:36 -0600

    added all funtions to week 10 assingmentA

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+
+/*this is a simple do mth function although it is a little bit bugged, as the mutiplication factor is actually just adding into the num1 and num2*/
+
+
+
+function do_math(num1,num2){
+    console.log(num1 + num2 *5)
+}
+
+do_math(5,5);
+
+/*this is the code for the secod function and it is working as intended after mutliple tests*/
+
+function read_it(string){
+    if(string[length]>10){
+        console.log(`this is shorter than 10 characters`)
+    }else{
+        console.log(`this is longer than 10 characters`)
+    }
+}
+
+read_it(`this is an example of a long sentence.`)
+
+
+/*this is the coding for the 3rd bonus function with the finding the first ph in the list*/
+
+
+function give_it(array){
+    let counter=0;
+    while(array=10){
+        console.log(startswith(ph))
+        counter+1
+    }
+
+}
+
+give_it[`phone`,`forest`,`forger`,`forgot`,`fillament`, `phoney`,`phil`, `phonky`, `phosphorous`,`freaky`]
